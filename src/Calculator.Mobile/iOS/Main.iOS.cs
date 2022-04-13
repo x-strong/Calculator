219976cbd6dd05bccc63fd4fcce92217a78d4e92
Jerome Laban <jerome.laban@nventive.com>
2022-04-13 17:10:41 -0400

    ci: adjut for invalid neutral culture

--- src/Calculator.Mobile/iOS/Main.iOS.cs
+++ src/Calculator.Mobile/iOS/Main.iOS.cs
@@ -8,6 +8,14 @@ namespace Calculator
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
+#if NET6_0_OR_GREATER
+			// https://github.com/xamarin/xamarin-macios/issues/14740
+			if (System.Threading.Thread.CurrentThread.CurrentCulture.IsNeutralCulture)
+			{
+				System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+				System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+			}
+#endif
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, typeof(App));
